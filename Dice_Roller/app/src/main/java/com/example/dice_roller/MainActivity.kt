6f3b7e10a0360184c780fbcc2703e4b26c70a511
Letícia <125403978+Lehguanaes@users.noreply.github.com>
2024-03-21 21:44:54 -0300

    Update MainActivity.kt

--- Dice_Roller/app/src/main/java/com/example/dice_roller/MainActivity.kt
+++ Dice_Roller/app/src/main/java/com/example/dice_roller/MainActivity.kt
@@ -55,7 +55,6 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
     val imageResource = when(result) {
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
-
         3 -> R.drawable.dice_3
         4 -> R.drawable.dice_4
         5 -> R.drawable.dice_5
